d07f4f3aedba8eed7fe32df8c939d99054c9e69c
damineesaini <damineesaini11@gmail.com>
2021-03-09 11:50:24 +0530

    added RestfulWebService2 assignment

--- RestfulWebServices2/src/main/java/com/restApi2/RestfulWebServices2/ContentNegotiation/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.restApi2.RestfulWebServices2.ContentNegotiation;
-
-public class UserNotFoundException extends Throwable {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
