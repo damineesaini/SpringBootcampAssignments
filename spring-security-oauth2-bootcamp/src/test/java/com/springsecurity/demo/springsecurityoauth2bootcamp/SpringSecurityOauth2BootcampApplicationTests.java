9f6d6ee5008c1d76eb16b720829caf7849e83cd9
damineesaini <damineesaini11@gmail.com>
2021-03-17 14:53:16 +0530

    Added oauth2 assignment

---
+++ spring-security-oauth2-bootcamp/src/test/java/com/springsecurity/demo/springsecurityoauth2bootcamp/SpringSecurityOauth2BootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.demo.springsecurityoauth2bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauth2BootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
