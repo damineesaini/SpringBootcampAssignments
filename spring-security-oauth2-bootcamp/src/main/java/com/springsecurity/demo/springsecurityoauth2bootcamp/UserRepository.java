95d2c92b2d0d91b48f244ed9f33ba4981e1d1349
damineesaini <damineesaini11@gmail.com>
2021-03-17 14:40:35 +0530

    update

--- spring-security-oauth2-bootcamp/src/main/java/com/springsecurity/demo/springsecurityoauth2bootcamp/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springsecurity.demo.springsecurityoauth2bootcamp;
-
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface UserRepository extends CrudRepository<UserEntity,Integer> {
-    UserEntity findByUsername(String username);
-}
